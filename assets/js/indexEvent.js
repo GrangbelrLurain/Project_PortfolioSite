73fea339efc2e7fcb29450f6234121ab4f3afff5
GrangbelrLurain <pcaso5@naver.com>
2021-01-06 11:33:23 +0900

    EVENT ASIDE CLICK REFECTORING

--- assets/js/indexEvent.js
+++ assets/js/indexEvent.js
@@ -12,73 +12,32 @@
   const mainNav = document.querySelectorAll('#aside .main > li > a');
   const allNav = document.querySelectorAll('#aside .main .sub li a')
   let href = null;
+
+  // SUBNAV EVENT HANDELER ADD
   allNav.forEach((elem) => {
     elem.addEventListener('click', (event) => {
       event.preventDefault();
       href = elem.getAttribute('href')
-      activeToggle(elem);
+      navScroll()
     })
   })
 
+  // MAINNAV EVENT HANDELER ADD
   mainNav.forEach((elem) => {
     elem.addEventListener('click', (event) => {
       event.preventDefault();
       href = elem.getAttribute('href')
-      navToggle(elem);
+      navScroll()
     })
   })
-  
-  const navToggle = (elem) => {
-    mainNav.forEach((findElem) => {
-      const findParent = findElem.parentElement
-      const parentElem = elem.parentElement;
-      if(findParent != parentElem){
-        findParent.classList.remove('active');
-        closeHeight(findParent);
-      } else {
-        if(parentElem.classList == 'active'){
-          parentElem.classList.remove('active');
-            closeHeight(parentElem);
-        } else {
-          parentElem.classList.add('active');
-          openHeight(parentElem);
-        };
-      }
-    })
-  }
 
-  const openHeight = (parentElem) => {
-    const sub = parentElem.querySelector('.sub');
-    if(sub){
-      sub.style.height = `${heightGet(parentElem)}px`
-    };
+  // NAV CLICK => SCROLL EVENT
+  const navScroll = () => {
+    nowContents = href;
+    scrollMove();
+    scrollBarUpdate();
+    navUpdate();
   }
-
-  const closeHeight = (parentElem) => {
-    const sub = parentElem.querySelector('.sub');
-    if(sub){
-      sub.style.height = `0px`;
-    }
-  }
-
-  const heightGet = (parentElem) => {
-    let height = 0;
-    parentElem.querySelectorAll('.sub li').forEach((innerElem) => {
-      height += innerElem.clientHeight;
-    })
-    return height
-  }
-
-  const activeToggle = (elem) => {
-    allNav.forEach((innerElem) => {
-      if(innerElem != elem){
-        innerElem.classList.remove('active');
-      } else {
-        elem.classList.add('active');
-      }
-    })
-  }
-
 
 
   // SCROLL EVENT
@@ -133,12 +92,12 @@
   const scrollEventWheel = () => {
     if(directionY > 0 && nowContents < contentsList.length - 1){
       nowContents++;
-      scrollDown();
+      scrollMove();
       scrollBarUpdate();
       navUpdate();
     } else if (directionY < 0 && nowContents > 0){
       nowContents--;
-      scrollUp();
+      scrollMove();
       scrollBarUpdate();
       navUpdate();
     }
@@ -147,22 +106,18 @@
   const scrollEventTouch = () => {
     if(directionY > window.innerHeight/4 && nowContents < contentsList.length - 1){
       nowContents++
-      scrollDown();
+      scrollMove();
       scrollBarUpdate();
       navUpdate();
     } else if (directionY < -window.innerHeight/4 && nowContents > 0){
       nowContents--
-      scrollUp();
+      scrollMove();
       scrollBarUpdate();
       navUpdate();
     }
   }
 
-  const scrollDown = () => {
-    contents.style.transform = `translateY(${-nowContents*100}vh)`
-  }
-  
-  const scrollUp = () => {
+  const scrollMove = () => {
     contents.style.transform = `translateY(${-nowContents*100}vh)`
   }
 
